7aee5ab616d03b3e279b11cc273bd5ed6f20b230
Paige Bolduc <paigebolduc@gmail.com>
2016-02-03 15:44:14 -0800

    create trade funcationality added, view for seeing pending trades offered to you

--- routes/profile.js
+++ routes/profile.js
@@ -7,6 +7,7 @@ var authMiddleware = require('../config/auth');
 
 var User = require('../models/user');
 var Book = require('../models/book');
+var Trade = require('../models/trade');
 
 router.use(authMiddleware);
 
@@ -62,18 +63,41 @@ router.get('/availableBooks', function(req, res){
 
 router.get('/tradeScreen/:bookId', function(req, res) {
   Book.findById(req.params.bookId, function(err, book){
-  console.log(book);
     if (err) return res.status(400).send(err);
     Book.find({userId: req.user._id, available: true}, function(err, userBooks){
-      console.log(userBooks);
       if (err) return res.status(400).send(err);
       res.render('tradeScreen', {book:book, userBooks: userBooks});
     });
   });
 });
 
+router.post('/createOffer', function(req, res) {
+  var trade = new Trade();
+  trade.offeredBook = req.body.offeredBookId;
+  trade.desiredBook  = req.body.desiredBookId;
+  Book.findById(trade.offeredBook, function(err, offerBook){
+    if (err) return res.status(400).send("error finding offered book",err);
+    trade.offeringUser = offerBook.userId;
+    Book.findById(trade.desiredBook, function(err, desireBook){
+      if (err) return res.status(400).send("error finding desired book", err);
+      trade.receivingUser = desireBook.userId;
+      trade.save(function(err, savedTrade) {
+        if (err) return res.status(400).send("error saving", err);
+        Trade.findById(savedTrade._id, function(err, newTrade) {
+          res.status(200).send(savedTrade);
+        });
+      });
+    });
+  });
+});
 
-
+router.get('/transactions', function(req, res) {
+  Trade.find({receivingUser: req.user._id, status: 'pending'}, function(err, receivedTrades) {
+    if (err) return res.status(400).send("error finding recieved trades",err);
+    res.render('transactions', {receivedTrades: receivedTrades});
+  }).populate('offeredBook desiredBook offeringUser');
+  // res.render('transactions', 'stuff');
+})
 
 
 module.exports = router;
