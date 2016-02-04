ce55e7b6f73fbb4d0f763517d1bcd1cfed4077d3
Paige Bolduc <paigebolduc@gmail.com>
2016-02-03 20:24:56 -0800

    sort user books ascending and descending

--- public/js/userBooks.js
+++ public/js/userBooks.js
@@ -2,8 +2,15 @@
 
 $(document).ready(init);
 
+var $rows;
+
 function init() {
   $('table').on('click', '#changeStatus', toggleStatus);
+  $rows = $('.dataRow');
+  $('table').on('click', '#title', sortByTitle);
+  $('table').on('click', '#author', sortByAuthor);
+  $('table').on('click', '#genre', sortByGenre);
+  $('table').on('click', '#available', sortByAvailable);
 };
 
 function toggleStatus(){
@@ -18,4 +25,32 @@ function toggleStatus(){
   }).fail(function(err){
     console.log("err ", err);
   })
+}
+
+function sortByTitle() {
+  var $this = $(this);
+  sortby('.title', $this);
+}
+function sortByAuthor() {
+  var $this = $(this);
+  sortby('.author', $this);
+}
+function sortByGenre() {
+  var $this = $(this);
+  sortby('.genre', $this);
+}
+function sortByAvailable() {
+  var $this = $(this);
+  sortby('.available', $this);
+}
+
+function sortby(field, $this) {
+	var mult = $this.data('sort');
+	$rows.sort(function(a,b) {
+		var aVal = $(a).children(field).text().toLowerCase();
+		var bVal = $(b).children(field).text().toLowerCase();
+		return ((aVal<bVal) ? -1 : 1) * mult;
+	});
+	$this.data('sort', -mult);
+	$('tbody').empty().append($rows);
 }
